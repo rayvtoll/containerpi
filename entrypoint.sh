2abc33f84d8f5df14c51271ffea1b712f6866294
Ray van Toll <48050007+rayvtoll@users.noreply.github.com>
2019-07-14 00:25:20 +0200

    user origin while sending key no longer needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ useradd -m -s /bin/bash $USER ; echo $USER:$USER | chpasswd
 rm -rf /home/$USER/.ssh/*
 mkdir -p /home/$USER/.ssh
 ssh-keygen -b 2048 -t rsa -f /home/$USER/.ssh/id_rsa -q -N ""
-curl -X POST app-provider/key -H 'content-type: application/json' -d '{"key":"'$(sed -E 's/ /%20/g' /home/$USER/.ssh/id_rsa.pub)'","user":"'$USER'"}'
+curl -X POST app-provider/key -H 'content-type: application/json' -d '{"key":"'$(sed -E 's/ /%20/g' /home/$USER/.ssh/id_rsa.pub)'"}'
 
 mv /tmp/tempuser/.config /home/$USER/.config
 cp /etc/skel/.* /home/$USER/
