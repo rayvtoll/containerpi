41589ceaab1bbcfe4eada97359772d8d60fbb7a9
rayvtoll <rayvtoll@gmail.com>
2019-07-23 11:20:38 +0200

    newer image

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,17 @@ useradd -m -s /bin/bash $USER ; echo $USER:$USER | chpasswd
 rm -rf /home/$USER/.ssh/*
 mkdir -p /home/$USER/.ssh
 ssh-keygen -b 2048 -t rsa -f /home/$USER/.ssh/id_rsa -q -N ""
-curl -X POST app-provider/key -H 'content-type: application/json' -d '{"key":"'$(sed -E 's/ /%20/g' /home/$USER/.ssh/id_rsa.pub)'"}'
+#curl -X POST app-provider/key -H 'content-type: application/json' -d '{"key":"'$(sed -E 's/ /%20/g' /home/$USER/.ssh/id_rsa.pub)'","user":"'$USER'"}'
 
-mv /tmp/tempuser/.config /home/$USER/.config
-cp /etc/skel/.* /home/$USER/
+#mv /tmp/tempuser/.config /home/$USER/.config
+#for i in $(ls /etc/skel) ; do cp -r $i /home/$USER/ ; done
+cp -r /etc/skel/. /home/$USER/
+#cp -r /etc/skel/.config /home/$USER/
 HOME=/home/$USER
 
 chown -R $USER:$USER /home/$USER
-tail -f /dev/null
+
+echo "rm -f /entrypoint.sh" >> run.sh
+echo "tail -f /dev/null" >> run.sh
+chmod +x run.sh
+sh run.sh
