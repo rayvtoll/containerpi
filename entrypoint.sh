a7b9865990dae35144765e522fafd37fb385f461
rayvtoll <rayvtoll@gmail.com>
2019-08-02 10:08:06 +0200

    udevd entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 #/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf &
 #useradd -m -s /bin/bash $USER ; echo $USER:$USER | chpasswd
 
-rm -rf /home/$USER/.ssh/*
-mkdir -p /home/$USER/.ssh
+rm -rf /root/.ssh/*
+mkdir -p /root/.ssh
 ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
-#curl -X POST app-provider/key -H 'content-type: application/json' -d '{"key":"'$(sed -E 's/ /%20/g' /home/$USER/.ssh/id_rsa.pub)'","user":"'$USER'"}'
+
+/lib/systemd/systemd-udevd --debug &
+udevadm trigger
+startx
 
 #mv /tmp/tempuser/.config /home/$USER/.config
 #for i in $(ls /etc/skel) ; do cp -r $i /home/$USER/ ; done
